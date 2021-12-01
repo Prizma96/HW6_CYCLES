8bdd6355775597f9e24a990df53e2355700098b6
Dasha <dashuta.tkachenko@list.ru>
2021-12-01 17:15:58 +0300

    correct commit

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -11,7 +11,7 @@ class SQRServiceTest {
     @CsvSource(value = {"'range from 200 to 300', 200, 300, 3",
             "'range from 100 to 10000', 10, 10000, 90",
             "'range from 10 to 99', 10, 99, 0",})
-    public void shouldSqrCounter(String testName,int from, int to, int expected) {
+    public void shouldSqrCounter(String ignoredTestName, int from, int to, int expected) {
         SQRService sqrService = new SQRService();
 
         int actual = sqrService.sqrCounter(from, to);
