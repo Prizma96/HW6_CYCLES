902b02b797efd0a1c0833348bfa4f89ce71b17ba
Dasha <dashuta.tkachenko@list.ru>
2021-12-01 17:19:59 +0300

    correct commit

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -8,9 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SQRServiceTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"'range from 200 to 300', 200, 300, 3",
-            "'range from 100 to 10000', 10, 10000, 90",
-            "'range from 10 to 99', 10, 99, 0",})
+    @CsvSource(value = {"'range from 200 to 300', 200, 300, 3", "'range from 100 to 10000', 10, 10000, 90", "'range from 10 to 99', 10, 99, 0",})
     public void shouldSqrCounter(String ignoredTestName, int from, int to, int expected) {
         SQRService sqrService = new SQRService();
 
